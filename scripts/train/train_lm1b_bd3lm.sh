ee32261c858a3018f2920b7b8e8755c747852fab
Sharvani Sinha <58387148+sharvani0102@users.noreply.github.com>
2025-11-23 18:30:19 -0500

    Update train_lm1b_bd3lm.sh

--- scripts/train/train_lm1b_bd3lm.sh
+++ scripts/train/train_lm1b_bd3lm.sh
@@ -4,10 +4,10 @@
 #SBATCH -e watch_folder/%x_%j.err     # log file (out & err)
 #SBATCH -N 1                          # Total number of nodes requested
 #SBATCH --get-user-env                # retrieve the users login environment
-#SBATCH --mem=32G                  # server memory requested (per node)
+#SBATCH --mem=32G                     # server memory requested (per node)
 #SBATCH -t 960:00:00                  # Time limit (hh:mm:ss)
-#SBATCH --partition=gpu          # Request partition
-#SBATCH --constraint="[a5000|a6000|3090|a100]"
+#SBATCH --partition=gpu               # Request partition
+#SBATCH --constraint="a5000|a6000|3090|a100" # CORRECTED CONSTRAINT (OR operator)
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4                  # Type/number of GPUs needed
 #SBATCH --open-mode=append            # Do not overwrite logs
